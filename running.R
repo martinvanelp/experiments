81a4cf916f8c5a543271bc803e5da1f2e86818f7
martinvanelp <martin.vanelp@gmail.com>
2014-08-27 11:28:20 +0200

    added a run

--- running.R
+++ running.R
@@ -1,7 +1,7 @@
 # simple lap speed calculator
 
 dist <- c(0, 3.34, 4.44, 1.27)
-time <- c(0, 19 + 48/60, 27 + 54/60, 7 + 39/60)
+time <- c(0, 19 + 10/60, 27 + 20/60, 7 + 48/60)
 
 dist <- round(dist, digits = 2)
 time <- round(time, digits = 2)
@@ -17,6 +17,11 @@ print(table)
 
 ##
 ## archive
+##
+#  2014/08/27
+#  dist <- c(0, 3.34, 4.44, 1.27)
+#  time <- c(0, 19 + 10/60, 27 + 20/60, 7 + 48/60)
+#
 ##
 #  2014/08/23
 #  dist <- c(0, 3.34, 4.44, 1.27)
