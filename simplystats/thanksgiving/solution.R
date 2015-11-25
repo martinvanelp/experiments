3afd9f41bfb4e871ea5030d73c32fa4803531113
martinvanelp <martin.vanelp@gmail.com>
2015-11-25 22:04:59 +0100

    init simplystats puzzle thanksgiving

---
+++ simplystats/thanksgiving/solution.R
@@ -0,0 +1,6 @@
+library(dplyr)
+
+tday    <- read.table("tday.delim", header=TRUE)
+puzzleL <- read.table("puzzle_nov25_2015.delim", header=TRUE)
+puzzleH <- read.table("puzzlehard_nov25_2015.delim", header=TRUE)
+
