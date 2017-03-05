1d04b3d690c1b7fd77aa5451efda01ab015418fa
Martin van Elp <martin.vanelp@gmail.com>
2017-03-05 10:54:25 +0100

    moved running data to csv

--- running.R
+++ running.R
@@ -1,68 +1,21 @@
+library(dplyr)
+library(knitr)
+
 # simple lap speed calculator
+runs <- read.csv2("running.csv", stringsAsFactors = FALSE)
 
-dist <- c(0, 3.34, 1.84, 1.72)
-time <- c(0, 19 + 23/60, 10 + 37/60, 10 + 26/60)
+totals <- 
+        group_by(runs, run, date) %>%
+        summarize_each(funs(sum)) %>%
+        ungroup() %>% mutate(section = "Total") %>%
+        mutate(minutes = minutes + as.integer(seconds/60),
+               seconds = seconds - as.integer(seconds/60) * 60)
 
-dist <- round(dist, digits = 2)
-time <- round(time, digits = 2)
+data <- rbind(runs, totals) %>% arrange(run, section) %>%
+        mutate(time  = minutes + seconds / 60,
+               speed = dist / time * 60) %>%
+        mutate(time  = round(time,2),
+               speed = round(speed,2))
 
-dist[1] <- sum(dist)
-time[1] <- sum(time)
-
-speed <- round(dist / time * 60, digits = 2)
-
-table <- cbind(dist, time, speed)
-
-print(table)
-
-##
-## ARCHIVE
-##
-## 2016/03/28
-#  dist <- c(0, 3.34, 1.84, 1.72)
-#  time <- c(0, 19 + 23/60, 10 + 37/60, 10 + 26/60)
-#
-## 2016/03/06
-#  dist <- c(0, 3.34, 1.84, 1.72)
-#  time <- c(0, 19 + 7/60, 10 + 39/60, 9 + 58/60)
-#
-## 2016/01/31
-#  dist <- c(0, 3.34, 1.84, 1.72)
-#  time <- c(0, 19 + 19/60, 10 + 46/60, 10 + 14/60)
-#
-## 2015/05/05
-#  dist <- c(0, 3.34, 1.84, 1.72)
-#  time <- c(0, 18 + 59/60, 10 + 48/60, 10 + 54/60)
-#
-## 2014/11/30
-#  dist <- c(0, 3.34, 1.84, 1.72)
-#  time <- c(0, 19 + 37/60, 11 + 37/60, 10 + 10/60)
-#
-## 2014/11/02
-#  dist <- c(0, 3.34, 1.84, 1.72)
-#  time <- c(0, 19 + 07/60, 10 + 54/60, 9 + 55/60)
-#
-## 2014/10/29
-#  dist <- c(0, 3.34, 1.84, 1.72)
-#  time <- c(0, 18 + 04/60, 10 + 16/60, 9 + 39/60)
-#
-## 2014/10/22
-#  dist <- c(0, 3.34, 1.96, 1.60)
-#  time <- c(0, 17 + 50/60, 10 + 24/60, 8 + 48/60)
-#
-## 2014/08/27
-#  dist <- c(0, 3.34, 4.44, 1.27)
-#  time <- c(0, 19 + 10/60, 27 + 20/60, 7 + 48/60)
-#
-## 2014/08/23
-#  dist <- c(0, 3.34, 4.44, 1.27)
-#  time <- c(0, 19 + 48/60, 27 + 54/60, 7 + 39/60)
-#
-## 2014/08/13
-#  dist <- c(0, 3.34, 1.84, 1.72)
-#  time <- c(0, 19 + 44/60, 10 + 56/60, 10 + 16/60)
-#
-## 2014/08/10
-#  dist <- c(0, 3.34, 1.84, 1.72)
-#  time <- c(0, 19 + 4/60, 10 + 55/60, 10 + 26/60)
-#
+kable(data %>% filter(section == "Total"))
+kable(data %>% filter(run > (max(run)-5)))
