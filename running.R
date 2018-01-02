50c725e5c7b353679d3984b3b1de5a18c8042b7c
Martin van Elp <martin.vanelp@gmail.com>
2018-01-02 15:25:15 +0100

    update running

--- running.R
+++ running.R
@@ -1,8 +1,11 @@
+#
+# Simple running analyzer
+#
+
 library(dplyr)
 library(knitr)
 
-# simple lap speed calculator
-runs <- read.csv2("running.csv", stringsAsFactors = FALSE)
+runs <- read.csv2(".\\experiments\\running.csv", stringsAsFactors = FALSE)
 
 totals <- 
         group_by(runs, run, date) %>%
@@ -12,10 +15,8 @@ totals <-
                seconds = seconds - as.integer(seconds/60) * 60)
 
 data <- rbind(runs, totals) %>% arrange(run, section) %>%
-        mutate(time  = minutes + seconds / 60,
-               speed = dist / time * 60) %>%
-        mutate(time  = round(time,2),
-               speed = round(speed,2))
+        mutate(time  = (minutes + seconds / 60) %>% round(2),
+               speed = (dist / time * 60) %>% round(2))
 
 kable(data %>% filter(section == "Total"))
 kable(data %>% filter(run > (max(run)-5)))
